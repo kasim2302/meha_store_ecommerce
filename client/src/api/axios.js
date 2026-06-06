5c004f8b9756e9161a9003d4572590a359c9e86d
Mohamed Kasim <kasim151000@gmail.com>
2026-06-06 18:06:08 +0530

    axios uri fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '',
-  withCredentials: true, // send httpOnly cookies automatically with every request
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default instance;
